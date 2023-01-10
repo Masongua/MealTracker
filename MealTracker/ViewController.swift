bb835c9fe97a7bb86eb8372f8bddb667f8cffcd5
Mason Guaman <masonstalin@gmail.com>
2023-01-10 13:02:54 -0500

    Initial Commit

---
+++ MealTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealTracker
+//
+//  Created by 5e on 1/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
